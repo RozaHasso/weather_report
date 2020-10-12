a9cc40b559d58efd3817ce159a39679be6d8f8eb
Nadeem Muhammad <31960661+mouhanadeem@users.noreply.github.com>
2020-10-12 11:39:20 +0200

    new model

--- static/view.js
+++ static/view.js
@@ -1,9 +1,6 @@
 export default window => {
     const document = window.document
     const table_body = document.getElementById('weather_data')
-    const listeners = []
-
-    const listen = l => listeners.push(l)
 
     const addData = p => {
         const tr = table_body.appendChild(document.createElement('tr'))
@@ -12,14 +9,11 @@ export default window => {
         tr.insertCell().appendChild(document.createTextNode(p.unit))
         tr.insertCell().appendChild(document.createTextNode(p.time))
         tr.insertCell().appendChild(document.createTextNode(p.place))
-        
-        
     }
 
-    const update = model => {
-      
-        model.prediction_equals().forEach(addData)
+    const update = data => {
+        data.weatherData().forEach(addData)
     }
- 
-    return { addData, update, listen, prompt }
+
+    return { addData, update }
 }
