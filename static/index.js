aab70fd63aeb120e5fd5d0ac093e600a5fe121d0
Nadeem Muhammad <31960661+mouhanadeem@users.noreply.github.com>
2020-10-13 18:50:24 +0200

    time and fields

--- static/index.js
+++ static/index.js
@@ -11,8 +11,8 @@
             request.onload = () => {
                 const data = request.response
                 //1.1) first query, •	All data for the latest measurement of weather data
-                var latestDate1 = new Date(Math.max.apply(null, data.map(e => { return new Date(e.time) })));
-                var latestData1 = data.filter(e => { var d = new Date(e.time); return d.getTime() == latestDate1.getTime() })
+                const latestDate1 = new Date(Math.max.apply(null, data.map(e => { return new Date(e.time) })));
+                const latestData1 = data.filter(e => { var d = new Date(e.time); return d.getTime() == latestDate1.getTime() })
                 //2) second query, •  Minimum temperature for the last 5 days
                 const last5Data = request.response
                 const for5Days = () => {
@@ -26,6 +26,7 @@
 
                 const finalformintemp = temp.reduce((op, { value, type, unit, time, place }) => {
                     const [date] = time.split('T', 1)
+                    time=time.split('T', 1)
                     op[date] = op[date] || { value: Infinity, type, unit, time, place }
                     op[date].value = op[date].value > value ? value : op[date].value
                     return op
@@ -34,6 +35,7 @@
                 //3) third query, •   Maximum temperature for the last 5 days
                 const finalformaxtemp = temp.reduce((op, { value, type, unit, time, place }) => {
                     const [date] = time.split('T', 1)
+                    time=time.split('T', 1)
                     op[date] = op[date] || { value: -Infinity, type, unit, time, place }
                     op[date].value = op[date].value < value ? value : op[date].value
                     return op
@@ -42,21 +44,22 @@
                 //4) forth query, •   Total precipitation for the last 5 days
 
                 const pre = last5daysObject.filter(t => t.type === 'precipitation')
-                var tmppre = {};
+                const tmppre = {};
                 pre.forEach(function (item) {
                     // if property for current date already exists  we update existing otherwise start new one
-                    var obj = tmppre[item.time.split('T', 1)] = tmppre[item.time.split('T', 1)] || { total: 0, type: item.type, unit: item.unit, time: item.time, place: item.place }
+                    var obj = tmppre[item.time.split('T', 1)] = tmppre[item.time.split('T', 1)] || { total: 0, type: item.type, precipitation_type: item.precipitation_type,unit: item.unit, time: item.time, place: item.place }
                     obj.total += item.value
                 });
 
                 const finalfortotalpre = Object.entries(tmppre).map(function (entry) {
-                    return { value: entry[1].total, type: entry[1].type, unit: entry[1].unit, time: entry[0], place: entry[1].place }
+                    return { value: entry[1].total, type: entry[1].type, precipitation_type: entry[1].precipitation_type, unit: entry[1].unit, time: entry[0], place: entry[1].place }
                 })
 
                 // 6) sixt query, •   Dominant wind direction for the last 5 days
                 const windspeed = last5daysObject.filter(t => t.type === 'wind speed')
                 const finalforwinddirection = windspeed.reduce((op, { value, direction, type, unit, time, place }) => {
                     const [date] = time.split('T', 1)
+                    time=time.split('T', 1)
                     op[date] = op[date] || { value: -Infinity, direction, type, unit, time, place }
                     op[date].value = op[date].value < value ? value : op[date].value
                     return op
@@ -96,13 +99,13 @@
                         var tmpwind = {};
                         wind.forEach(function (item) {
                             // if property for current date already exists  we update existing otherwise start new one
-                            var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, unit: item.unit, time: item.time, place: item.place }
+                            var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, direction: item.direction, unit: item.unit, time: item.time, place: item.place }
                             obj.count++
                             obj.total += item.value
                         });
 
                         const finalfifthavgwindHor = Object.entries(tmpwind).map(function (entry) {
-                            return { value: entry[1].total / entry[1].count, type: entry[1].type, unit: entry[1].unit, time: entry[0], place: entry[1].place }
+                            return { value: entry[1].total / entry[1].count, type: entry[1].type, direction: entry[1].direction ,unit: entry[1].unit, time: entry[0], place: entry[1].place }
                         })
 
 
@@ -117,13 +120,13 @@
                             var tmpwind = {};
                             wind.forEach(function (item) {
                                 // if property for current date already exists  we update existing otherwise start new one
-                                var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, unit: item.unit, time: item.time, place: item.place }
+                                var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, direction: item.direction, unit: item.unit, time: item.time, place: item.place }
                                 obj.count++
                                 obj.total += item.value
                             });
 
                             const finalfifthavgwindAar = Object.entries(tmpwind).map(function (entry) {
-                                return { value: entry[1].total / entry[1].count, type: entry[1].type, unit: entry[1].unit, time: entry[0], place: entry[1].place }
+                                return { value: entry[1].total / entry[1].count, type: entry[1].type, direction: entry[1].direction, unit: entry[1].unit, time: entry[0], place: entry[1].place }
                             })
 
 
@@ -137,13 +140,13 @@
                                 var tmpwind = {};
                                 wind.forEach(function (item) {
                                     // if property for current date already exists  we update existing otherwise start new one
-                                    var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, unit: item.unit, time: item.time, place: item.place }
+                                    var obj = tmpwind[item.time.split('T', 1)] = tmpwind[item.time.split('T', 1)] || { count: 0, total: 0, type: item.type, direction: item.direction, unit: item.unit, time: item.time, place: item.place }
                                     obj.count++
                                     obj.total += item.value
                                 });
 
                                 const finalfifthavgwindCop = Object.entries(tmpwind).map(function (entry) {
-                                    return { value: entry[1].total / entry[1].count, type: entry[1].type, unit: entry[1].unit, time: entry[0], place: entry[1].place }
+                                    return { value: entry[1].total / entry[1].count, type: entry[1].type, direction: entry[1].direction, unit: entry[1].unit, time: entry[0], place: entry[1].place }
                                 })
 
 
